8b3532bcf5583c497c03d203cbe5454c80ce3f8a
Ahmed-AlShaiba <amashaiba@gmail.com>
2024-04-24 12:13:35 +0400

    Added tailwincss, fonts,  and color pallete. also added pages controller

---
+++ config/tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'light': "#FFFCF2",
+        'beige': "#CCC5B9",
+        'pdark': "#252422",
+        'ldark': "#403D39",
+        'porange': "#EB5E28",
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
